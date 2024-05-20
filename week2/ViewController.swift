8ec6a055bd73080d8c51400d8eea88c2b0230406
Hardik Paranjape <hardik@Hardiks-MacBook-Pro.local>
2024-05-20 18:53:13 -0400

    push the project

--- week2/ViewController.swift
+++ week2/ViewController.swift
@@ -9,6 +9,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var calculator: UILabel!
+    
+    @IBOutlet weak var numberone: UITextField!
+    
+    @IBOutlet weak var numbertwo: UITextField!
+    
+    @IBOutlet weak var calculatedvalue: UITextField!
+    
+    //buttons
+    
+    @IBAction func allclear(_ sender: UIButton) {
+    }
+    
+    @IBAction func plusminus(_ sender: UIButton) {
+    }
+
+    @IBAction func exponent(_ sender: UIButton) {
+    }
+    
+    @IBAction func divide(_ sender: UIButton) {
+    }
+    
+    //number buttons
+    
+    @IBAction func seven(_ sender: Any) {
+    }
+    
+    
+    @IBAction func eight(_ sender: Any) {
+    }
+    
+    @IBAction func nine(_ sender: Any) {
+    }
+    
+    @IBAction func multiply(_ sender: Any) {
+    }
+    
+    @IBAction func four(_ sender: Any) {
+    }
+    
+    @IBAction func five(_ sender: Any) {
+    }
+    
+    @IBAction func six(_ sender: Any) {
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+    }
+    
+    @IBAction func one(_ sender: Any) {
+    }
+    
+    @IBAction func two(_ sender: Any) {
+    }
+    
+    @IBAction func three(_ sender: Any) {
+    }
+    
+    @IBAction func plus(_ sender: Any) {
+    }
+    
+    @IBAction func zero(_ sender: Any) {
+    }
+    
+    @IBAction func decimal(_ sender: Any) {
+    }
+    
+    @IBAction func equal(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
